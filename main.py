278453001547aada6c43f9902260250f36435252
Denis <deniblackman2@gmail.com>
2026-02-27 13:49:16 +0500

    Fix Railway postgres URL

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base, Session, relationship
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
